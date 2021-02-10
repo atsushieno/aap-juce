0c1bbe3abe0139f6c159f973b539eb3bc228fd2a
atsushieno <atsushieno@gmail.com>
2021-02-10 20:27:33 +0900

    Rename juceaap_audio_plugin_processors -> juceaap_audio_processors.
    We will have to rewrite aap-juce-plugin-host and aap-juce-augene to
    reflect this change.

--- modules/juceaap_audio_processors/juceaap_audio_plugin_processors.h
+++ modules/juceaap_audio_processors/juceaap_audio_plugin_processors.h
@@ -1,12 +1,12 @@
 /*
 BEGIN_JUCE_MODULE_DECLARATION
 
-ID: juceaap_audio_plugin_processors
+ID: juceaap_audio_processors
 vendor: atsushieno
 version: 0.1.0
 name: AAP JUCE hosting support
 description: Classes for AAP hosting support
-website: https://github.com/atsushieno/android-audio-plugin-framework
+website: https://github.com/atsushieno/aap-juce
 license: GPLv3
 
 END_JUCE_MODULE_DECLARATION
